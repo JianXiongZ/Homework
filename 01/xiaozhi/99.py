1ae276d70f172ff4d990a7ac64579d2fd581d9fa
jianxiong <jianxiong@canaan-creative.com>
2017-03-23 18:22:58 +0800

    Modified master content

--- 01/xiaozhi/99.py
+++
@@ -1,6 +0,0 @@
-#encoding:utf-8
-
-for x in range(1, 10):
-    for y in range(1, x+1):
-        print("{}*{}={}".format(x,y, (x*y)), end=" ")
-    print("  ")
--- 01/zhanghuangchen/MultiTable9By9.py
+++
@@ -1,8 +0,0 @@
-# encoding:utf-8
-
-for i in range(1,10):
-	for j in range(1,i+1):
-		if j != i:
-		    print(i, '*', j, '=', i*j,'\t', end = ' ')
-		else:
-			print(i, '*', j, '=', i*j)
